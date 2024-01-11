c0318822eeb83e1cb6e718af72bfbe8088a778ad
Arun <aruway333@gmail.com>
2024-01-10 22:47:11 -0500

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import css from './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList=()=>{
+    return(
+        <section className='bookList'>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    )
+};
+
+const Book=()=>{
+    return(
+        <div className='book'>
+            <Image/>
+            <Title/>
+            <Author/>
+        </div>
+    );
+};
+
+const Image=()=><img src="https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg" alt="" />;
+const Title=()=><h3>Atomic Habits</h3> ;
+const Author=()=> <h4>James Clear</h4> ;
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList />);
